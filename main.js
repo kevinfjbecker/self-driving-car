0b7fb6020cd15b74f506c470699e21ed427c00ba
Kevin Becker <kevinfjbecker@gmail.com>
2024-05-10 09:51:59 +0200

    Lesson 2

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ canvas.height = window.innerHeight
 canvas.width = 200
 
 const ctx = canvas.getContext('2d')
-const car = new Car(100, 100, 30, 50)
+const road = new Road( canvas.width / 2, canvas.width * 0.9, 3 )
+const startLaneIndex = 1
+const car = new Car(road.getLaneCenter( startLaneIndex ), 100, 30, 50)
 car.draw(ctx)
 
 animate( )
@@ -13,6 +15,13 @@ function animate( )
     car.update( )
 
     canvas.height = window.innerHeight
+
+    ctx.save( )
+    ctx.translate( 0, - car.y + canvas.height * 0.7 )
+
+    road.draw( ctx )
     car.draw( ctx )
+
+    ctx.restore( )
     requestAnimationFrame( animate )
 }
