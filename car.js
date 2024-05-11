c13db5e3efe471025a88ed085d0784094720b6fe
Kevin Becker <kevinfjbecker@gmail.com>
2024-05-11 12:28:37 +0200

    Lesson 3

--- car.js
+++ car.js
@@ -13,12 +13,14 @@ class Car
         this.friction = 0.05
         this.angle = 0
 
+        this.sensor = new Sensor( this )
         this.controls = new Controls( )
     }
 
-    update( )
+    update( roadBorders )
     {
         this.#move( )
+        this.sensor.update( roadBorders )
     }
 
     #move( )
@@ -88,5 +90,7 @@ class Car
         ctx.fill( )
 
         ctx.restore( )
+
+        this.sensor.draw( ctx )
     }
 }
