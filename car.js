194e5d1441e5b1794b6b821f400be69f03d13f75
KevinOmega365 <kevinfjbecker@gmail.com>
2024-05-14 15:33:17 +0200

    Lesson 5 (wip)

--- car.js
+++ car.js
@@ -1,6 +1,6 @@
 class Car
 {
-    constructor( x, y, width, height )
+    constructor( x, y, width, height, controlType, maxSpeed = 3 )
     {
         this.x = x
         this.y = y
@@ -9,32 +9,47 @@ class Car
 
         this.speed = 0
         this.acceleration = 0.2
-        this.maxSpeed = 3
+        this.maxSpeed = maxSpeed
         this.friction = 0.05
         this.angle = 0
         this.polygon = []
         this.damaged = false
 
-        this.sensor = new Sensor( this )
-        this.controls = new Controls( )
+        if( controlType !== 'DUMMY')
+        {
+            this.sensor = new Sensor( this )
+        }
+        console.log(this.sensor)
+        this.controls = new Controls( controlType )
     }
 
-    update( roadBorders )
+    update( roadBorders, traffic )
     {
         if( ! this.damaged )
         {
             this.#move( )
             this.polygon = this.#createPolygon( )
-            this.damaged = this.#assessDamage( roadBorders )
+            this.damaged = this.#assessDamage( roadBorders, traffic )
         }
-        this.sensor.update( roadBorders )
+        if( this.sensor )
+        {
+            this.sensor.update( roadBorders )
+        }
     }
 
-    #assessDamage( roadBorders )
+    #assessDamage( roadBorders, traffic )
     {
         for( let i = 0; i < roadBorders.length; i++ )
         {
             if( polysIntersect( this.polygon, roadBorders[ i ] ) )
+            {
+                return true
+            }
+        }
+
+        for( let i = 0; i < traffic.length; i++ )
+        {
+            if( polysIntersect( this.polygon, traffic[ i ].polygon ) )
             {
                 return true
             }
@@ -143,6 +158,9 @@ class Car
         }
         ctx.fill( )
 
-        this.sensor.draw( ctx )
+        if( this.sensor )
+        {
+            this.sensor.draw( ctx )
+        }
     }
 }
--- sensor.js
+++ sensor.js
@@ -5,7 +5,7 @@ class Sensor
         this.car = car
         this.rayCount = 5
         this.rayLength = 150
-        this.raySpread = Math.PI / 4
+        this.raySpread = Math.PI / 2
 
         this.rays = [ ]
         this.readings = [ ]
