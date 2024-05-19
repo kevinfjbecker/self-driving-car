b068a41d6c3adb8f0c7fcf7f50f0d75f5c161158
Kevin Becker <kevinfjbecker@gmail.com>
2024-05-19 12:06:39 +0200

    Lesson 8 (wip)

--- car.js
+++ car.js
@@ -156,7 +156,7 @@ class Car
         this.y -= Math.cos( this.angle ) * this.speed
     }
 
-    draw( ctx, color )
+    draw( ctx, color, drawSensor = false )
     {
         if( this.damaged )
         {
@@ -175,7 +175,7 @@ class Car
         }
         ctx.fill( )
 
-        if( this.sensor )
+        if( this.sensor && drawSensor )
         {
             this.sensor.draw( ctx )
         }
--- main.js
+++ main.js
@@ -10,13 +10,8 @@ const networkCtx = networkCanvas.getContext( '2d' )
 const road = new Road( carCanvas.width / 2, carCanvas.width * 0.9, 3 )
 
 const startLaneIndex = 1
-const car = new Car(
-    road.getLaneCenter( startLaneIndex ),
-    100,
-    30,
-    50,
-    'AI' // 'KEYS'
-) 
+const N = 100
+const cars = generateCars( N )
 
 const traffic = [
     new Car(
@@ -31,31 +26,64 @@ const traffic = [
 
 animate( )
 
+function generateCars( N )
+{
+    const cars = [ ]
+    for( let i = 0; i < N; i ++ )
+    {
+        cars.push( new Car(
+            road.getLaneCenter( startLaneIndex ),
+            100,
+            30,
+            50,
+            'AI' // 'KEYS'
+        ) )
+    }
+    return cars
+}
+
 function animate( time )
 {
     for( let i = 0; i < traffic.length; i ++ )
     {
         traffic[ i ].update( road.borders, [ ] )
     }
-    car.update( road.borders, traffic )
+    
+    for( let i = 0; i < cars.length; i ++ )
+    {
+        cars[ i ].update( road.borders, traffic )
+    }
+
+    const bestCar = cars.find( car =>
+        car.y === Math.min( ... cars.map( c => c.y ) )
+    )
 
     carCanvas.height = window.innerHeight
     networkCanvas.height = window.innerHeight
 
     carCtx.save( )
-    carCtx.translate( 0, - car.y + carCanvas.height * 0.7 )
+    carCtx.translate( 0, - bestCar.y + carCanvas.height * 0.7 )
 
     road.draw( carCtx )
+    
     for( let i = 0; i <traffic.length; i ++ )
     {
         traffic[ i ].draw( carCtx, 'red' )
     }
-    car.draw( carCtx, 'blue' )
+
+    carCtx.globalAlpha = 0.2
+    for( let i = 1; i < cars.length; i ++ )
+    {
+        cars[ i ].draw( carCtx, 'blue' )
+    }
+    carCtx.globalAlpha = 1
+
+    bestCar.draw( carCtx, 'blue', true )
 
     carCtx.restore( )
 
     networkCtx.lineDashOffset = - time * 0.01
-    Visualizer.drawNetwork( networkCtx, car.brain )
+    Visualizer.drawNetwork( networkCtx, bestCar.brain )
 
     requestAnimationFrame( animate )
 }
