dd4e1dc05fd4683eb612e6e2745ec77bfbc5a3a6
Kevin Becker <kevinfjbecker@gmail.com>
2024-05-20 12:02:27 +0200

    Lesson 8 (complete)

--- main.js
+++ main.js
@@ -9,22 +9,42 @@ const networkCtx = networkCanvas.getContext( '2d' )
 
 const road = new Road( carCanvas.width / 2, carCanvas.width * 0.9, 3 )
 
-const startLaneIndex = 1
 const N = 100
 const cars = generateCars( N )
+let bestCar = cars[ 0 ]
+if( localStorage.getItem( 'bestBrain' ) )
+{
+    for(let i = 0; i < cars.length; i ++)
+    {
+        cars[ i ].brain = JSON.parse( localStorage.getItem( 'bestBrain' ) )
+        if( i !== 0 )
+        {
+            NeuralNetwork.mutate(cars[ i ].brain, 0.2)
+        }
+    }
+}
 
 const traffic = [
-    new Car(
-        road.getLaneCenter( startLaneIndex ),
-        -100,
-        30,
-        50,
-        'DUMMY',
-        2
-    )
+    new Car(road.getLaneCenter( 1 ),-100,30,50,'DUMMY',2),
+    new Car(road.getLaneCenter( 0 ),-300,30,50,'DUMMY',2),
+    new Car(road.getLaneCenter( 2 ),-300,30,50,'DUMMY',2),
+    new Car(road.getLaneCenter( 0 ),-500,30,50,'DUMMY',2),
+    new Car(road.getLaneCenter( 1 ),-500,30,50,'DUMMY',2),
+    new Car(road.getLaneCenter( 1 ),-700,30,50,'DUMMY',2),
+    new Car(road.getLaneCenter( 2 ),-700,30,50,'DUMMY',2),
 ]
 
 animate( )
+
+function save( )
+{
+    localStorage.setItem( 'bestBrain', JSON.stringify( bestCar.brain ) )
+}
+
+function discard( )
+{
+    localStorage.removeItem( 'bestBrain' )
+}
 
 function generateCars( N )
 {
@@ -32,7 +52,7 @@ function generateCars( N )
     for( let i = 0; i < N; i ++ )
     {
         cars.push( new Car(
-            road.getLaneCenter( startLaneIndex ),
+            road.getLaneCenter( 1 ),
             100,
             30,
             50,
@@ -54,7 +74,7 @@ function animate( time )
         cars[ i ].update( road.borders, traffic )
     }
 
-    const bestCar = cars.find( car =>
+    bestCar = cars.find( car =>
         car.y === Math.min( ... cars.map( c => c.y ) )
     )
 
